894112ed9ce4976ccec2c9209632f42d2e7b4b3a
Amit Maurya <76053686+amit-inf01@users.noreply.github.com>
2021-07-22 00:17:01 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    resetToken:String,
+    expireToken:Date,
+    pic:{
+     type:String,
+     default:'https://res.cloudinary.com/diuqey8fu/image/upload/v1626379311/no-user-image_mernaa.gif'
+    },
+    followers:[{type:ObjectId,ref:"User"}],
+    following:[{type:ObjectId,ref:"User"}]
+})
+
+mongoose.model("User",userSchema)
